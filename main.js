a7f591b0d5a9c260f5c78ed4fb1a24f4aad9a68c
jasmijn301 <454893@student.fontys.nl>
2021-12-23 14:51:23 +0100

    cleaned up my code
    I cleaned up my code by deleting the timeOut, changing some names and adding space and comments between the code.

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const myVideo = document.getElementById("myVideo");
 const btnPlay = document.getElementById("btnPlay");
 const btnPause = document.getElementById("btnPause");
-const timeOut = document.getElementById("timeOut");
 const vidNumOut = document.getElementById("vidNum");
-let timer = null;
 
 //buttons
 btnPlay.addEventListener("click", playPause);
@@ -12,6 +10,7 @@ btnNext1.addEventListener("click", nextVideo1);
 btnNext2.addEventListener("click", nextVideo2);
 myVideo.addEventListener("ended", vidEnded);
 
+//visibility
 document.getElementById("btnPause").style.visibility = "hidden";
 document.getElementById("nextButtons").style.visibility = "hidden";
 document.getElementById("clockitself").style.visibility = "hidden";
@@ -20,11 +19,22 @@ document.getElementById("clockitself").style.visibility = "hidden";
 document.getElementById("btnNext1").style.background = "white";
 document.getElementById("btnNext2").style.background = "skyblue";
 
+//animation
 var timesUp = document.getElementById("pointer");
-timesUp.addEventListener("animationend", myEndFunction);
+timesUp.addEventListener("animationend", animationEnds);
 
-function myEndFunction(event) {
+//videos
+const vids = ["1.mp4", "1.5.mp4", "2.mp4", "2.5.mp4", "3.mp4", "3.5.mp4", "4.mp4", "5.mp4", "5.5.mp4", "6.mp4", "7.mp4"];
+let vidPlaying = 0;
+
+let choice = 1;
+// choice 1 means option 1 is chosen, choice 2 means option 2 is chosen.
+
+//This function is called upon when the stopwatch stops
+function animationEnds(event) {
+
     console.log("end");
+    
     if (choice == 1) {
         //check which video is playing and direct to the video of choice
         //if the last video is playing reset the buttons
@@ -34,14 +44,12 @@ function myEndFunction(event) {
             document.getElementById("btnNext2").style.visibility = "visible";
             btnNext1.innerHTML = "option 1";
             vidPlaying = 0;
-            timer = null;
             document.getElementById("nextButtons").style.visibility = "visible";
-            document.getElementById("timeOut").style.visibility = "visible";
         } else {
             nextVideo2();
         }
         myVideo.src = vids[vidPlaying];
-        vidNumOut.innerHTML = (vidPlaying) + "/9";
+        vidNumOut.innerHTML = (vidPlaying) + "/10";
 
         playVideo();
     } else if (choice == 2) {
@@ -59,7 +67,7 @@ function myEndFunction(event) {
             vidPlaying = 0;
         }
         myVideo.src = vids[vidPlaying];
-        vidNumOut.innerHTML = (vidPlaying) + "/9";
+        vidNumOut.innerHTML = (vidPlaying) + "/10";
 
         playVideo();
     } else {
@@ -67,19 +75,11 @@ function myEndFunction(event) {
     }
 }
 
-//videos
-const vids = ["1.mp4", "1.5.mp4", "2.mp4", "2.5.mp4", "3.mp4", "3.5.mp4", "4.mp4", "5.mp4", "5.5.mp4", "6.mp4", "7.mp4"];
-let vidPlaying = 0;
-
-let choice = 1;
-// choice 1 means option 1 is chosen, choice 2 means option 2 is chosen.
-
 function playPause(event) {
 
     switch (event.target.id) {
         case "btnPlay":
             playVideo();
-            timer = setInterval(update, 100);
             document.getElementById("btnPlay").style.visibility = "hidden";
             document.getElementById("btnPause").style.visibility = "visible";
             //make sure the stopwatch starts/continues when play is pressed
@@ -101,55 +101,27 @@ function playVideo() {
 
     myVideo.play();
 
-    // update every 100 ms the time being displayed
-    timer = setInterval(update, 100);
-
-    hidden();
+    visibility();
 }
 
-function update() {
-
-    timeOut.innerHTML = "Time: " + myTime(myVideo.currentTime) + "/" + myTime(myVideo.duration);
-}
-
-function hidden() {
+function visibility() {
 
     //the option buttons should be hidden at the first part of each fragment and with the end vids
     if (vidPlaying == 0 || vidPlaying == 2 || vidPlaying == 4 || vidPlaying == 6 || vidPlaying == 7 || vidPlaying == 9 || vidPlaying == 10) {
         document.getElementById("nextButtons").style.visibility = "hidden";
-        document.getElementById("timeOut").style.visibility = "hidden";
         document.getElementById("clockitself").style.visibility = "hidden";
     } else {
         document.getElementById("nextButtons").style.visibility = "visible";
-        document.getElementById("timeOut").style.visibility = "visible";
         document.getElementById("pointer").style.animationPlayState = "running";
         document.getElementById("clockitself").style.visibility = "visible";
     }
 }
 
-//make time readable for humans
-function myTime(time) {
-
-    var hr = ~~(time / 3600);
-    var min = ~~((time % 3600) / 60);
-    var sec = time % 60;
-    var sec_min = "";
-    if (hr > 0) {
-        sec_min += "" + hrs + ":" + (min < 10 ? "0" : "");
-    }
-    sec_min += "" + min + ":" + (sec < 10 ? "0" : "");
-    sec_min += "" + Math.round(sec);
-    return sec_min;
-}
-
 function vidEnded() {
-
-    clearInterval(timer);
-    timeOut.innerHTML = "Timer: 0";
 
     //make sure if the player doesn't pick an option, the video still continues
     if (vidPlaying == 0 || vidPlaying == 1 || vidPlaying == 2 || vidPlaying == 3 || vidPlaying == 4 || vidPlaying == 7 || vidPlaying == 8) {
-        myEndFunction();
+        animationEnds();
     } else {
         document.getElementById("btnNext1").style.visibility = "visible";
         document.getElementById("btnNext2").style.visibility = "hidden";
@@ -157,21 +129,20 @@ function vidEnded() {
     }
 }
 
+//this video is called upon when the player chooses option 1 or don't choose anything at all
 function nextVideo1() {
 
     choice = 1;
     document.getElementById("btnNext1").style.background = "white";
     document.getElementById("btnNext2").style.background = "skyblue";
-
 }
 
+//this video is called upon when the player chooses option 2
 function nextVideo2() {
 
     choice = choice + 1;
     document.getElementById("btnNext1").style.background = "skyblue";
     document.getElementById("btnNext2").style.background = "white";
-
-
 }
 
 function removeButton() {
