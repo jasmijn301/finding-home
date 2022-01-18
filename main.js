a44c471113ca142ef612243cb3b23d2bba988c0a
jasmijn301 <454893@student.fontys.nl>
2022-01-18 20:29:55 +0100

    bloopers and replay
    I added an option to watch bloopers, in making this work I discovered partly why the replay element did not do what it was supposed to do.
    
    I also deleted an unnecessary function ( removeButton() )

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const myVideo = document.getElementById("myVideo");
 const btnPlay = document.getElementById("btnPlay");
-const vidNumOut = document.getElementById("vidNum");
 
 //buttons
 btnPlay.addEventListener("click", playPause);
@@ -19,7 +18,7 @@ var timesUp = document.getElementById("pointer2");
 timesUp.addEventListener("animationend", animationEnds);
 
 //videos
-const vids = ["1.mp4", "1.5.mp4", "2.mp4", "2.5.mp4", "3.mp4", "4.mp4", "5.mp4", "5.5.mp4", "6.mp4", "7.mp4"];
+const vids = ["1.mp4", "1.5.mp4", "2.mp4", "2.5.mp4", "3.mp4", "4.mp4", "5.mp4", "5.5.mp4", "6.mp4", "7.mp4", "8.mp4"];
 let vidPlaying = 0;
 
 let choice = 1;
@@ -27,7 +26,7 @@ let choice = 1;
 let play = 0;
 //0 means the video is paused, 1 means the video is playing.
 
-//This function is called upon when the stopwatch stops
+//This function is called upon when the stopwatch stops or when a video stops (except for the end vids)
 function animationEnds(event) {
 
     if (choice == 1) {
@@ -35,13 +34,6 @@ function animationEnds(event) {
         //if the last video is playing reset the buttons
         if (vidPlaying == 0 || vidPlaying == 1 || vidPlaying == 2 || vidPlaying == 3 || vidPlaying == 6 || vidPlaying == 7) {
             vidPlaying = vidPlaying + 1;
-            // } else if (vidPlaying == 4 || vidPlaying == 5 || vidPlaying == 8 || vidPlaying == 9) {
-            //     document.getElementById("btnNext2").style.visibility = "visible";
-            //     btnNext1.innerHTML = "option 1";
-            //     vidPlaying = 0;
-            //     play = 0;
-            //     choice = 1;
-            // document.getElementById("nextButtons").style.visibility = "visible";
         } else {
             console.log("error: animationEnds, choice1")
         }
@@ -56,16 +48,10 @@ function animationEnds(event) {
             choice = 1;
         } else if (vidPlaying == 3) {
             vidPlaying = 5;
-            removeButton();
+            document.getElementById("nextButtons").style.visibility = "hidden";
         } else if (vidPlaying == 7) {
             vidPlaying = 9;
-            removeButton();
-            // } else if (vidPlaying == 4 || vidPlaying == 5 || vidPlaying == 8 || vidPlaying == 9) {
-            //     vidPlaying = 0;
-            //     document.getElementById("btnNext2").style.visibility = "visible";
-            //     btnNext1.innerHTML = "Option 1";
-            //     play = 0;
-            //     choice = 1;
+            document.getElementById("nextButtons").style.visibility = "hidden";
         } else {
             console.log("error: animationEnds, choice2")
         }
@@ -104,7 +90,7 @@ function playVideo() {
 function visibility() {
 
     //the option buttons should be hidden at the first part of each fragment and with the end vids
-    if (vidPlaying == 0 || vidPlaying == 2 || vidPlaying == 4 || vidPlaying == 5 || vidPlaying == 6 || vidPlaying == 8 || vidPlaying == 9) {
+    if (vidPlaying == 0 || vidPlaying == 2 || vidPlaying == 4 || vidPlaying == 5 || vidPlaying == 6 || vidPlaying == 8 || vidPlaying == 9 || vidPlaying == 10 ) {
         document.getElementById("nextButtons").style.visibility = "hidden";
         document.getElementById("clockitself").style.visibility = "hidden";
         document.getElementById("clockitself2").style.visibility = "hidden";
@@ -126,22 +112,31 @@ function vidEnded() {
     //make sure if the player doesn't pick an option, the video still continues
     if (vidPlaying == 0 || vidPlaying == 1 || vidPlaying == 2 || vidPlaying == 3 || vidPlaying == 6 || vidPlaying == 7) {
         animationEnds();
+    } else if (vidPlaying == 4 || vidPlaying == 5 || vidPlaying == 8 || vidPlaying == 9) {
+        document.getElementById("nextButtons").style.visibility = "visible";
+        btnNext1.innerHTML = "Replay";
+        btnNext2.innerHTML = "Watch bloopers";
+    } else if (vidPlaying == 10) {
+        btnNext1.innerHTML = "Replay music video";
+        btnNext2.innerHTML = "Replay bloopers";
+        document.getElementById("nextButtons").style.visibility = "visible";
     } else {
-        document.getElementById("btnNext1").style.visibility = "visible";
-        document.getElementById("btnNext2").style.visibility = "hidden";
-        btnNext1.innerHTML = "replay";
+        console.log("error: vidEnded()")
     }
 }
 
-//this video is called upon when the player chooses option 1 or don't choose anything at all
+//this function is called upon when the player chooses option 1 or doesn't choose anything at all
 function nextVideo1() {
 
-    if (vidPlaying == 4 || vidPlaying == 5 || vidPlaying == 8 || vidPlaying == 9) {
+    if (vidPlaying == 4 || vidPlaying == 5 || vidPlaying == 8 || vidPlaying == 9 || vidPlaying == 10) {
         vidPlaying = 0;
-        document.getElementById("btnNext2").style.visibility = "visible";
+        document.getElementById("nextButtons").style.visibility = "hidden";
         btnNext1.innerHTML = "Option 1";
+        btnNext2.innerHTML = "Option 2";
+        btnPlay.innerHTML = "play";
         play = 0;
         choice = 1;
+        myVideo.src = vids[vidPlaying];
     } else if (vidPlaying == 1 || vidPlaying == 3 || vidPlaying == 7) {
         choice = 1;
         document.getElementById("btnNext2").style.width = "90%";
@@ -153,19 +148,26 @@ function nextVideo1() {
     }
 }
 
-//this video is called upon when the player chooses option 2
+//this function is called upon when the player chooses option 2
 function nextVideo2() {
 
-    choice = choice + 1;
-    document.getElementById("btnNext1").style.width = "90%";
-    document.getElementById("btnNext1").style.marginLeft = "5%";
-    document.getElementById("btnNext2").style.width = "100%";
-    document.getElementById("btnNext2").style.marginLeft = "0%";
-}
-
-function removeButton() {
-
-    //change the option buttons when you reach one of the end vids
-    document.getElementById("btnNext2").style.visibility = "hidden";
-    btnNext1.innerHTML = "replay";
+    if (vidPlaying == 4 || vidPlaying == 5 || vidPlaying == 8 || vidPlaying == 9) {
+        vidPlaying = 10;
+        document.getElementById("nextButtons").style.visibility = "hidden";
+        play = 1;
+        choice = 1;
+        myVideo.src = vids[vidPlaying];
+        playVideo();
+    } else if (vidPlaying == 1 || vidPlaying == 3 || vidPlaying == 7) {
+        choice = choice + 1;
+        document.getElementById("btnNext1").style.width = "90%";
+        document.getElementById("btnNext1").style.marginLeft = "5%";
+        document.getElementById("btnNext2").style.width = "100%";
+        document.getElementById("btnNext2").style.marginLeft = "0%";
+    } else if(vidPlaying == 10) {
+        playVideo();
+        document.getElementById("nextButtons").style.visibility = "hidden";
+    } else {
+        console.log("error: nextVideo2");
+    }
 }
